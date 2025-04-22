440f7259c39302e06fe9c71d4d4af1dff5445c75
Bobcho101 <bgbobi878@gmail.com>
2025-04-22 22:17:40 +0300

    changed the document title

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from 'next/app';
 import Navigation from '@/components/Navigation';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Mobile BG</title>
+        <meta name="description" content="Mobile BG is your destination for discovering and buying cars you truly love." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Navigation />
       <main style={{ padding: '1rem' }}>
         <Component {...pageProps} />
