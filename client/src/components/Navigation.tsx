70ca1afbb1c7bde97695efbb1a2a25b10571559a
Bobcho101 <bgbobi878@gmail.com>
2025-04-22 22:08:14 +0300

    changed the routes render structuture

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,16 +1,35 @@
 import Link from "next/link";
 
+const routes = [
+    {
+        name: 'Home',
+        path: '/',
+    },
+    {
+        name: 'Cars for Sale',
+        path: '/catalog',
+    },
+    {
+        name: 'About',
+        path: '/about',
+    },
+    {
+        name: 'Contact',
+        path: '/contact',
+    },
+];
+
 export default function Navigation() {
+
     return (
         <header className="bg-blue-600 text-white">
             <div className="max-w-7xl mx-auto px-6 py-4">
                 <div className="flex items-center justify-between">
                 <div className="text-xl font-bold">Mobile BG</div>
                 <nav className="space-x-4">
-                    <Link href="/" className="hover:text-blue-200">Home</Link>
-                    <Link href="" className="hover:text-blue-200">Cars for Sale</Link>
-                    <Link href="/about" className="hover:text-blue-200">About</Link>
-                    <Link href="#contact" className="hover:text-blue-200">Contact</Link>
+                    {routes.map((route, index) => (
+                        <Link key={index} href={route.path} className="hover:text-blue-200">{route.name}</Link>
+                    ))}
                 </nav>
                 </div>
             </div>
