c7cab39dd84b77f33e247a676b84f3d2fac9d704
Bobcho101 <bgbobi878@gmail.com>
2025-05-08 13:50:47 +0300

    added useFetch hook (that i might not use cuz of server side rendering)

---
+++ client/src/api/carsApi.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+const baseUrl: string = "http://localhost:3030/data/cars";
+
+export const useFetchCars = () => {
+    const [ cars, setCars ] = useState([]);
+    const [ loading, setLoading ] = useState<boolean>(false);
+    const [ error, setError ] = useState<null | string>(null);
+
+    useEffect(() => {
+        const fetchCars = async () => {
+            setLoading(true);
+            try{
+                const response = await fetch(baseUrl);
+                const data = await response.json();
+                console.log(cars);
+                
+                setCars(data);
+            } catch(err: any){
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchCars();
+    }, [cars]);
+
+    return {
+        cars,
+        loading,
+        error,
+    }
+};
