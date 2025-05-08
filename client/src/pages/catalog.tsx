f66cb632a9b7c1d557dd6065c804139ae832c2a5
Bobcho101 <bgbobi878@gmail.com>
2025-05-08 13:50:58 +0300

    made the catalog with server side rendering

--- client/src/pages/catalog.tsx
+++ client/src/pages/catalog.tsx
@@ -10,87 +10,104 @@ interface Car {
     engine: string;
     price: number;
 };
+
 export const getServerSideProps = async () => {
-    const cars: Car[] = [
-        {
-            src: 'https://images.unsplash.com/photo-1603216934532-6d98729f9c60',
-            brand: 'Mercedes-Benz',
-            model: 'C-Class',
-            type: 'Luxury Sedan',
-            transmission: 'Automatic',
-            engine: 'Petrol',
-            price: 42000,
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be',
-            brand: 'Ford',
-            model: 'Mustang GT',
-            type: 'Muscle Car',
-            transmission: 'Manual',
-            engine: 'Petrol',
-            price: 39000,
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1549924231-f129b911e442',
-            brand: 'Honda',
-            model: 'Civic',
-            transmission: 'Automatic',
-            type: 'Compact Sedan',
-            engine: 'Gas',
-            price: 22500,
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1583267741379-c715a693f879',
-            brand: 'Toyota',
-            model: 'Supra',
-            transmission: 'Manual',
-            type: 'Performance Coupe',
-            engine: 'Petrol',
-            price: 50000,
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1604335399103-74fb1e5c1574',
-            brand: 'Chevrolet',
-            model: 'Camaro',
-            type: 'Sports Car',
-            transmission: 'Manual',
-            engine: 'Diesel',
-            price: 28000,
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1588617358531-bc64e8f48f4d',
-            brand: 'Subaru',
-            model: 'WRX',
-            type: 'AWD Sedan',
-            engine: 'Gas',
-            transmission: 'Automatic',
-            price: 33000,
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1592363187340-f828a6f60c3f',
-            brand: 'Tesla',
-            model: 'Model 3',
-            type: 'Electric Sedan',
-            engine: 'Electric',
-            transmission: 'Manual',
-            price: 38000,
-          },
-          {
-            src: 'https://images.unsplash.com/photo-1615933406712-10dff34c50a9',
-            brand: 'BMW',
-            model: 'i4',
-            type: 'Electric Coupe',
-            engine: 'Electric',
-            transmission: 'Automatic',
-            price: 55000,
-        },
-    ];
-  
-    return {
-      props: {
-        cars,
-      },
-    };
+    const baseUrl: string = "http://localhost:3030/data/cars";
+
+    try{
+        const response = await fetch(baseUrl); 
+        const data = await response.json();
+
+        return {
+            props: {
+              cars: data, 
+            },
+        };
+    } catch(err: any){
+        console.log(err.message);
+    }
+
+
+    // const cars: Car[] = [
+    //     {
+    //         src: 'https://images.unsplash.com/photo-1603216934532-6d98729f9c60',
+    //         brand: 'Mercedes-Benz',
+    //         model: 'C-Class',
+    //         type: 'Luxury Sedan',
+    //         transmission: 'Automatic',
+    //         engine: 'Petrol',
+    //         price: 42000,
+    //       },
+    //       {
+    //         src: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be',
+    //         brand: 'Ford',
+    //         model: 'Mustang GT',
+    //         type: 'Muscle Car',
+    //         transmission: 'Manual',
+    //         engine: 'Petrol',
+    //         price: 39000,
+    //       },
+    //       {
+    //         src: 'https://images.unsplash.com/photo-1549924231-f129b911e442',
+    //         brand: 'Honda',
+    //         model: 'Civic',
+    //         transmission: 'Automatic',
+    //         type: 'Compact Sedan',
+    //         engine: 'Gas',
+    //         price: 22500,
+    //       },
+    //       {
+    //         src: 'https://images.unsplash.com/photo-1583267741379-c715a693f879',
+    //         brand: 'Toyota',
+    //         model: 'Supra',
+    //         transmission: 'Manual',
+    //         type: 'Performance Coupe',
+    //         engine: 'Petrol',
+    //         price: 50000,
+    //       },
+    //       {
+    //         src: 'https://images.unsplash.com/photo-1604335399103-74fb1e5c1574',
+    //         brand: 'Chevrolet',
+    //         model: 'Camaro',
+    //         type: 'Sports Car',
+    //         transmission: 'Manual',
+    //         engine: 'Diesel',
+    //         price: 28000,
+    //       },
+    //       {
+    //         src: 'https://images.unsplash.com/photo-1588617358531-bc64e8f48f4d',
+    //         brand: 'Subaru',
+    //         model: 'WRX',
+    //         type: 'AWD Sedan',
+    //         engine: 'Gas',
+    //         transmission: 'Automatic',
+    //         price: 33000,
+    //       },
+    //       {
+    //         src: 'https://images.unsplash.com/photo-1592363187340-f828a6f60c3f',
+    //         brand: 'Tesla',
+    //         model: 'Model 3',
+    //         type: 'Electric Sedan',
+    //         engine: 'Electric',
+    //         transmission: 'Manual',
+    //         price: 38000,
+    //       },
+    //       {
+    //         src: 'https://images.unsplash.com/photo-1615933406712-10dff34c50a9',
+    //         brand: 'BMW',
+    //         model: 'i4',
+    //         type: 'Electric Coupe',
+    //         engine: 'Electric',
+    //         transmission: 'Automatic',
+    //         price: 55000,
+    //     },
+    // ];
+
+    // return {
+    //   props: {
+    //     cars,
+    //   },
+    // };
 };
 
 const Catalog: React.FC<{ cars: Car[] }> = ({ cars }) => {
