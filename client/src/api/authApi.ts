ae023c15b1aa398f19ebc0e1511f76755ec62050
Bobcho101 <bgbobi878@gmail.com>
2025-05-09 22:11:06 +0300

    made login hook

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -38,3 +38,39 @@ export const useRegister = () => {
         error,
     }
 };
+
+export const useLogin = () => {
+    const [ loading, setLoading ] = useState(false);
+    const [ error, setError ] = useState<string | null>(null);
+
+    const login = async (email: string, password: string) => {
+        setLoading(true);
+
+        try{
+            const res = await fetch(`${baseUrl}login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            });
+
+            const authToken = await res.json();
+            return authToken;
+        } catch(err: any){
+            setError(err.message);
+            console.log(err.message);
+        } finally{
+            setLoading(false);
+        }
+    };
+
+    return {
+        login,
+        loading,
+        error,
+    }
+};
